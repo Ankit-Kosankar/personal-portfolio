ca88ad804d444bcc5e5900cb590410fefa0289eb
Ankit-Kosankar <53865903+Ankit-Kosankar@users.noreply.github.com>
2024-12-03 02:03:13 +0530

    added logo layout is ready basic one

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,37 @@
 <script>
   import '../styles/layout.css'; // Adjust the path as necessary
+  import '../styles/navbar.css';
+  import logo from '../lib/assets/images/skull.svg';
 </script>
 
-<div>
-  <header>
-    <nav>
-        <li><a href="#about">About</a></li>
-        <li><a href="#blog">Blog</a></li>
-        <li><a href="#resume">Resume</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact Me</a></li>
-    </nav>
-  </header>
+<div class="container">
 
-  <main>
-    <slot />
-  </main>
+  <div class="nav-container">
+    <header>
+      <div class="logo">
+        <img  class="logo-img" src={logo} alt="Logo"> <!-- Replace with actual logo path -->
+    </div>
+      <nav class="nav-bar">
+          <li><a href="about">About</a></li>
+          <li><a href="blog">Blog</a></li>
+          <li><a href="resume">Resume</a></li>
+          <li><a href="projects">Projects</a></li>
+          <li><a href="contact-me">Contact-Me</a></li>
+      </nav>
+    </header>
+  </div>
 
-  <footer>
-    <p>&copy; 2023 Your Name</p>
-  </footer>
+  <div class="content-container">
+    <div class="main-container">
+      <main>
+        <slot />  
+      </main>
+    </div>
+    
+    <div class="footer-container">
+      <footer>
+        <p>&copy; 2023 Your Name</p>
+      </footer>
+    </div>
+  </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import '../styles/home.css'; // Adjust the path as necessary
+  import profileImage from '../lib/assets/images/profile-pic.svg.svg'
+</script>
+
+<div class="profile-container">
+  <div class="profile-text">
+      <h1>Hi, I'm Ankit Kosankar, A Software Engineer and AI/ML Enthusiast.</h1>
+      <p>With a strong background in software development and </p>
+      <p>keen interest in artificial intelligence and machine learning, </p> 
+      <p>I thrive on solving complex problems and creating innovative solutions.</p>
+      <p>My experience includes working with various </p> 
+      <p>programming languages and AI frameworks.</p>
+  </div>
+  <div class="profile-image">
+      <img src={profileImage} alt="Ankit Kosankar" />
+  </div>
+</div>
